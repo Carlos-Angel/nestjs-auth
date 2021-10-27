5f9863cc5a7a8b73b3dc6c5a8f8a7d7782ab3e45
Charly_Angel <c.a.a.a.503@gmail.com>
2021-10-27 10:27:06 -0500

    feat(happy path): acción para el registro de usuarios

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {}
+
+  async registerUser(registerUserDto: RegisterUserDto): Promise<void> {
+    return this.userRepository.createUser(registerUserDto);
+  }
+}
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,12 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { RegisterUserDto } from './dto/register-user.dto';
 
-@EntityRepository()
-export class UserRepository extends Repository<User> {}
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async createUser(registerUserDto: RegisterUserDto): Promise<void> {
+    const { name, email, password } = registerUserDto;
+    const user = this.create({ name, email, password });
+    await this.save(user);
+  }
+}
