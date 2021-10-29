b6d1fee68d13b7f147878460a5116310eb55b23b
Charly_Angel <c.a.a.a.503@gmail.com>
2021-10-29 08:55:00 -0500

    feat: validación en los atributos de los endpoints

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsNotEmpty, IsString, Length, IsEmail } from 'class-validator';
+
 export class RegisterUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 20)
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @Length(6, 20)
   password: string;
 }
